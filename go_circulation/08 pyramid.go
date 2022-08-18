0a45326b8b825a4dfad4fb4d1a7e2861f1b2c290
tieyongjie <1042798703@qq.com>
2022-08-18 19:43:20 +0800

    alter

--- go_circulation/08 pyramid.go
+++ go_circulation/08 pyramid.go
@@ -2,10 +2,30 @@ package main
 
 import "fmt"
 
+// 打印半个个金字塔
+//func main() {
+//	for i := 0; i < 5; i++ {
+//		for j := 0; j <= i; j++ {
+//			fmt.Print("*")
+//		}
+//		fmt.Println()
+//	}
+//}
+
+// 打印完整的金字塔
 func main() {
-	for i := 0; i < 5; i++ {
-		for j := 0; j <= i; j++ {
-			fmt.Print("*")
+	var totallevel int = 20
+	for i := 1; i < totallevel; i++ {
+		for j := 1; j <= totallevel-i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= 2*i-1; j++ {
+			if j == 1 || j == 2*i-1 || i == totallevel-1 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+
 		}
 		fmt.Println()
 	}
