a0abe58961956637a6ac4c4d3d1831c27e821762
tieyongjie <1042798703@qq.com>
2022-12-09 16:50:12 +0800

    yongjie

--- go_circulation/08 pyramid.go
+++ go_circulation/08 pyramid.go
@@ -11,21 +11,66 @@ import "fmt"
 //		fmt.Println()
 //	}
 //}
-
+//
 // 打印完整的金字塔
+//func main() {
+//	var totallevel int = 10
+//	for i := 1; i < totallevel; i++ {
+//		for j := 1; j <= totallevel-i; j++ {
+//			fmt.Print(" ")
+//		}
+//		for j := 1; j <= 2*i-1; j++ {
+//			fmt.Print("*")
+//		}
+//		fmt.Println()
+//	}
+//}
+
+// 打印完整的空心金字塔
+//func main() {
+//	var totallevel int
+//	fmt.Println("请输入金字塔高度level:")
+//	fmt.Scanln(&totallevel)
+//	for i := 1; i < totallevel; i++ {
+//		for j := 1; j <= totallevel-i; j++ {
+//			fmt.Print(" ")
+//		}
+//		for j := 1; j <= 2*i-1; j++ {
+//			if j == 1 || j == 2*i-1 || i == totallevel-1 {
+//				fmt.Print("*")
+//			} else {
+//				fmt.Print(" ")
+//			}
+//		}
+//		fmt.Println()
+//	}
+//}
+
+//打印一个空心的菱形
 func main() {
-	var totallevel int = 20
+	var totallevel int
+	fmt.Println("请输入金字塔高度level:")
+	fmt.Scanln(&totallevel)
 	for i := 1; i < totallevel; i++ {
 		for j := 1; j <= totallevel-i; j++ {
 			fmt.Print(" ")
 		}
 		for j := 1; j <= 2*i-1; j++ {
-			if j == 1 || j == 2*i-1 || i == totallevel-1 {
+			if j == 1 || j == 2*i-1 {
 				fmt.Print("*")
 			} else {
 				fmt.Print(" ")
 			}
-
+		}
+		fmt.Println()
+	}
+	for i := totallevel; i < totallevel; i-- {
+		for j := 1; j <= 2*i-1; j++ {
+			if j == 1 || j == 2*i-1 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
 		}
 		fmt.Println()
 	}
